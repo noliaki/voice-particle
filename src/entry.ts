7cce85c2e5b5e31fba938dc5c692e5115a7e986b
noliaki <noliaki@gmail.com>
2018-08-04 23:21:43 +0900

    :tada: init commit

---
+++ src/entry.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+
+const App = new Vue({
+  render: h => h('#app')
+})
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack')
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+]
+
+const config = {
+  mode: process.env.NODE_ENV,
+  context: path.resolve('src'),
+  entry: 'entry.ts',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: { appendTsSuffixTo: [/\.vue$/] }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      }
+    ]
+  },
+  plugins,
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: process.env.NODE_ENV === 'production'
+          }
+        }
+      })
+    ]
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  config.watch = true
+  config.cache = true
+  config.plugins = plugins.concat([
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    })
+  ])
+}
+
+module.exports = config
